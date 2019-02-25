c3c56ed6bbe50377b82b623e776585a7044d187c
Chris De Gour <cdegour@cdegour-ltm2.internal.salesforce.com>
2019-02-25 12:46:04 -0800

    eh

---
+++ app.js
@@ -0,0 +1,26 @@
+const nforce = require('nforce');
+const fs = require('fs');
+const Kafka = require('no-kafka');
+
+fs.writeFileSync('./client.crt', process.env.KAFKA_CLIENT_CERT);
+fs.writeFileSync('./client.key', process.env.KAFKA_CLIENT_CERT_KEY);
+
+const org = nforce.createConnection({
+  clientId: process.env.CLIENTID,
+  clientSecret: process.env.CLIENTSECRET,
+  redirectUri: 'http://localhost:3000/oauth/_callback',
+  environment: process.env.ENVIRONMENT,
+  mode: 'single',
+});
+
+const producer = new Kafka.Producer({
+  connectionString: process.env.KAFKA_URL,
+  ssl: {
+    certFile: './client.crt',
+    keyFile: './client.key',
+  },
+});
+
+return producer.init().then(function(){
+    console.log('Producer Connected');
+});
